67347ea7e020dadeb1ae0e53b1a1157b142537e4
Bilal <your_email@example.com>
2025-11-25 03:39:35 -0800

    update

--- app/ClientProviders.tsx
+++ app/ClientProviders.tsx
@@ -3,9 +3,10 @@
 import { ReactNode } from "react";
 import { SessionProvider } from "next-auth/react";
 import { CartProvider } from "./context/CartContext";
-import Navbar from "./component/Navbar/Navbar";
+// import Navbar from "./component/Navbar/Navbar";
 import Footer from "./component/Footer/Footer";
 import { Toaster } from "react-hot-toast";
+import Navbar from "./component/Navbar/Navbar";
 
 interface Props {
   children: ReactNode;
